0ba2ef0ae0a5d59b7c66a8ec0a7020cc00763804
Sammy guttman <sguttman@itpmsoftware.com>
2024-02-15 16:11:00 -0500

    feat: set default language page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,11 @@
+import { headers } from 'next/headers'
+
 import { getDictionary } from "./[lang]/dictionaries";
 import { Home } from "./home";
 
 export default async function InitPage() {
-  const dict = await getDictionary("es")
+  const locale = headers().get('Accept-Language')?.includes('es') ? 'es' : 'en';
+
+  const dict = await getDictionary(locale)
   return <Home dict={dict} />;
 }
